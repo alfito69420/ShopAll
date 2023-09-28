1f88596a941d7d584c6edf67d264bb455636c213
Luis Gerardo López Ruiz <108818400+GeraTaker@users.noreply.github.com>
2023-09-28 05:04:13 -0600

    ADD: model user informtaion

--- src/main/java/com/example/metaphorce/modelos/User.java
+++ src/main/java/com/example/metaphorce/modelos/User.java
@@ -1,4 +1,21 @@
 package com.example.metaphorce.modelos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
+    private String nombre;
+    private String email;
+    private String contrasena;
+    private String direccion;
+    private String telefono;
+    private String token;
 }
