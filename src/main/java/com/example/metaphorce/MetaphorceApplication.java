d2fc897c622705a185f21035e3ea5901c57ae85d
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-09-27 22:44:07 -0600

    Initial Commit

---
+++ src/main/java/com/example/metaphorce/MetaphorceApplication.java
@@ -0,0 +1,13 @@
+package com.example.metaphorce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaphorceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaphorceApplication.class, args);
+	}
+
+}
