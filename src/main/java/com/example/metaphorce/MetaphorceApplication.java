fc881930c93367bf31368c133a4078729f2716a2
Luis Gerardo López Ruiz <108818400+GeraTaker@users.noreply.github.com>
2023-10-02 22:36:29 -0600

    ADD: ConnectionDB
    Se agrego una conexion a la base de datos a si mismo, se creo una clase de esta en conjunto, de en el archivo principal un ejemplo para saber si esta conectado oh no

--- src/main/java/com/example/metaphorce/MetaphorceApplication.java
+++ src/main/java/com/example/metaphorce/MetaphorceApplication.java
@@ -2,12 +2,30 @@ package com.example.metaphorce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.metaphorce.repository.ConnectionDB;
+
+import java.sql.Connection;
 
 @SpringBootApplication
 public class MetaphorceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MetaphorceApplication.class, args);
+
+
+
+		/*
+		*Test para la base de datos
+		*Es solo para comprobar la conexion a la base de datos
+		 */
+		ConnectionDB connectionDB = new ConnectionDB();
+		try {
+			//Para comprobar que esta cnectado
+			Connection connection = connectionDB.createConnection();
+			//Para cerrarlo
+			//connectionDB.closeConnection(connection);
+		} catch (Exception e) {
+			System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+		}
 	}
 
 }
