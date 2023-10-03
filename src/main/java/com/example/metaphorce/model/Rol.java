9218d10e13402cbec989350b5f60210b83e1c284
MarshalMadafaka <146318292+MarshalMadafaka@users.noreply.github.com>
2023-10-03 12:11:24 -0600

    Update Rol.java
    rol actualizacion 2

--- src/main/java/com/example/metaphorce/model/Rol.java
+++ src/main/java/com/example/metaphorce/model/Rol.java
@@ -1,1 +1,28 @@
+package com.example.metaphorce.model;
 
+public class Rol {
+
+    private int rol_id;
+    private String rol;
+
+    public Rol(int rol_id, String rol) {
+        this.rol_id = rol_id;
+        this.rol = rol;
+    }
+
+    public int getRol_id() {
+        return rol_id;
+    }
+
+    public void setRol_id(int rol_id) {
+        this.rol_id = rol_id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+} //close class
