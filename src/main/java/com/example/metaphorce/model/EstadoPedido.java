0010feae95dec7f6c389b62a054d4a934b3695c2
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-10-03 12:28:37 -0600

    REFACTOR: uso de lombok para la generacion de codigo en los modelos

--- src/main/java/com/example/metaphorce/model/EstadoPedido.java
+++ src/main/java/com/example/metaphorce/model/EstadoPedido.java
@@ -1,27 +1,13 @@
 package com.example.metaphorce.model;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class EstadoPedido {
     private Long estadoPedidoId;
     private String nombre;
-
-    public EstadoPedido(Long estadoPedidoId, String nombre) {
-        this.estadoPedidoId = estadoPedidoId;
-        this.nombre = nombre;
-    }
-
-    public Long getEstadoPedidoId() {
-        return estadoPedidoId;
-    }
-
-    public void setEstadoPedidoId(Long estadoPedidoId) {
-        this.estadoPedidoId = estadoPedidoId;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 } //close class
