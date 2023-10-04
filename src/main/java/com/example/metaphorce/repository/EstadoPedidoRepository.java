7fc3897a493fc68141da5535fef3b0ba67478499
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-10-03 19:03:18 -0600

    REFACTOR: anotacion de repositorio

--- src/main/java/com/example/metaphorce/repository/EstadoPedidoRepository.java
+++ src/main/java/com/example/metaphorce/repository/EstadoPedidoRepository.java
@@ -2,6 +2,8 @@ package com.example.metaphorce.repository;
 
 import com.example.metaphorce.model.EstadoPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstadoPedidoRepository extends JpaRepository<EstadoPedido, Long> {
 }
