f8df6beea678548730216ca2b283fe0d539169e5
Luis Gerardo López Ruiz <108818400+GeraTaker@users.noreply.github.com>
2023-10-05 06:24:42 -0600

    ADD APIS tienda

--- src/main/java/com/example/metaphorce/service/TiendaServices.java
+++ src/main/java/com/example/metaphorce/service/TiendaServices.java
@@ -5,9 +5,12 @@ import com.example.metaphorce.model.Categoria;
 import com.example.metaphorce.model.Tienda;
 import com.example.metaphorce.repository.TiendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -15,7 +18,7 @@ public class TiendaServices {
 
 
     private final TiendaRepository tiendaRepository;
-
+    HashMap<String,Object> datos = new HashMap<>();
     @Autowired
     public TiendaServices(TiendaRepository tiendaRepository){
         this.tiendaRepository = tiendaRepository;
@@ -34,8 +37,28 @@ public class TiendaServices {
     public List<Tienda> getTienda(){
         return tiendaRepository.findAll();
     }
+    public  ResponseEntity<Object> newTienda(Tienda tienda) {
 
-    public void newTienda(Tienda tienda) {
         this.tiendaRepository.save(tienda);
+        datos.put("Data", tienda);
+        datos.put("Message","Se pudo crear la tienda");
+        datos.put("Status",201);
+        datos.put("Flag","true");
+        if(tienda.getTienda_id() > 0){
+            datos.put("Message","Se pudo actualizar");
+        }
+        return new ResponseEntity<>(datos, HttpStatus.CREATED);
     };
+
+
+
+
+
+
+   /*
+    public void eliminar(long id){
+        this.tiendaRepository.deleteTiendaBy(id);
+    }
+
+    */
 }
