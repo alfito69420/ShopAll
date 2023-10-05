e098d50315dce0e65fc7d83a05a09ba7aa4a653f
Luis Gerardo López Ruiz <108818400+GeraTaker@users.noreply.github.com>
2023-10-05 06:24:56 -0600

    ADD APIS tiposPago

--- src/main/java/com/example/metaphorce/controller/TipoPagoController.java
+++ src/main/java/com/example/metaphorce/controller/TipoPagoController.java
@@ -1,10 +1,10 @@
 package com.example.metaphorce.controller;
 
+import com.example.metaphorce.model.Tienda;
 import com.example.metaphorce.model.TipoPago;
 import com.example.metaphorce.service.TipoPagoServices;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,15 @@ public class TipoPagoController {
     @GetMapping("/all")
     public List<TipoPago> getTipoPago(){
         return tipoPagoServices.getTipoPago();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> registrar(@RequestBody TipoPago tipoPago){
+        return this.tipoPagoServices.newTipoPago(tipoPago);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Object> actualizar(@RequestBody TipoPago tipoPago){
+        return this.tipoPagoServices.newTipoPago(tipoPago);
     }
 }
