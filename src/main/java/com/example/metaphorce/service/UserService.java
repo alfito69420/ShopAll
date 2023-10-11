474f8f20a86ef46d4c6be49270613be791ee3dd5
Luis Gerardo López Ruiz <108818400+GeraTaker@users.noreply.github.com>
2023-10-11 08:24:29 -0600

    UPDATE apis ventas-user

--- src/main/java/com/example/metaphorce/service/UserService.java
+++ src/main/java/com/example/metaphorce/service/UserService.java
@@ -1,37 +1,82 @@
 package com.example.metaphorce.service;
 
+import com.example.metaphorce.domain.UserResponse;
+import com.example.metaphorce.domain.UserResponse;
 import com.example.metaphorce.model.Tienda;
 import com.example.metaphorce.model.User;
+import com.example.metaphorce.model.User;
 import com.example.metaphorce.repository.UserRepository;
+
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
+
 
 @Service
 public class UserService {
     private  final UserRepository userRepository;
-    HashMap<String,Object> datos = new HashMap<>();
+    UserResponse response;
 
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
 
-    public List<User> getUser(){
-        return userRepository.findAll();
-    }
-
-    public ResponseEntity<Object>  newUser(User user) {
-        this.userRepository.save(user);
-        datos.put("Data", user);
-        datos.put("Message","Se pudo crear");
-        datos.put("Status",201);
-        datos.put("Flag","true");
-        if(user.getUsuario_id() > 0){
-            datos.put("Message","Se pudo actualizar");
+   public ResponseEntity<Object> getUser(){
+        List<User> users = userRepository.findAll();
+        if (!users.isEmpty()) {
+            response = new UserResponse(users, "Obtención de todas los Users", 200, true);
+            return new ResponseEntity<>(response.response2(), HttpStatus.OK);
+        } else {
+            response = new UserResponse("No se encontraron Users", 400, false);
+            return new ResponseEntity<>(response.response2(), HttpStatus.OK);
         }
-        return new ResponseEntity<>(datos, HttpStatus.CREATED);
+    }
+    public  ResponseEntity<Object> newUser(User user) {
+        this.userRepository.save(user);
+        response = new UserResponse(user, "Se pudo crear la User",200,true );
+        return new ResponseEntity<>(response.response(), HttpStatus.OK);
+
+
     };
+
+    public  ResponseEntity<Object> updateUser(Long id, User user) {
+        if (userRepository.findById(id).isPresent()) {
+            User existingUser = userRepository.findById(id).get();
+            existingUser.setNombre(user.getNombre());
+            //existingUser.setDescripcion(user.getDescripcion());
+            userRepository.save(existingUser);
+            response = new UserResponse(existingUser, "Se pudo actualizar", 200, true);
+            return new ResponseEntity<>(response.response(), HttpStatus.OK);
+        } else {
+            response = new UserResponse("No existe el ID: " + id, 400, false);
+            return new ResponseEntity<>(response.response(), HttpStatus.OK);
+        }
+    };
+
+    public ResponseEntity<Object>  eliminar(Long id){
+        //Verificar si esta vacio
+        if(!this.userRepository.findById(id).isEmpty()){
+            this.userRepository.deleteById(id);
+            response = new UserResponse("Si se pudo eliminar el ID :"+id,200,true );
+            return new ResponseEntity<>(response.response(),HttpStatus.OK);
+        }else{
+            response = new UserResponse("No existe el ID: "+id,400,false );
+            return new ResponseEntity<>(response.response(),HttpStatus.OK);
+
+        }
+    };
+
+    public ResponseEntity<Object> getOne(Long id){
+        if (userRepository.findById(id).isPresent()) {
+            User User = userRepository.findById(id).get();
+            response = new UserResponse(User, "Si encontró el ID: " + id, 200, true);
+            return new ResponseEntity<>(response.response(), HttpStatus.OK);
+        } else {
+            response = new UserResponse("No existe la User con el ID: " + id, 400, false);
+            return new ResponseEntity<>(response.response(), HttpStatus.OK);
+        }
+    }
 }
