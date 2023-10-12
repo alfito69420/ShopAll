d4462c4cbfd82073b1de07bf57dff3b4073a8a45
Alfredo Aramburo <20031030@itcelaya.edu.mx>
2023-10-12 13:25:30 -0600

    ADD: logs

--- src/main/java/com/example/metaphorce/service/VentaDetalleService.java
+++ src/main/java/com/example/metaphorce/service/VentaDetalleService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -15,6 +17,8 @@ public class VentaDetalleService {
 
     private final VentaDetalleRepository ventaDetalleRepository;
     private VentaDetalleResponse ventaDetalleResponse;
+    private static final Logger logger = LoggerFactory.getLogger(VentaDetalleService.class);
+
 
     @Autowired
     public VentaDetalleService(final VentaDetalleRepository ventaDetalleRepository) {
@@ -25,7 +29,8 @@ public class VentaDetalleService {
     public ResponseEntity<Object> getVentaDetalle() {
         List<VentaDetalle> ventaDetalleList = ventaDetalleRepository.findAll();
         if (!ventaDetalleList.isEmpty()) {
-            ventaDetalleResponse = new VentaDetalleResponse(ventaDetalleList, "Obtención de todas los registros del detalle de la venta", 200, true);
+            ventaDetalleResponse = new VentaDetalleResponse(ventaDetalleList,
+                    "Obtención de todas los registros del detalle de la venta", 200, true);
             return new ResponseEntity<>(ventaDetalleResponse.response2(), HttpStatus.OK);
         } else {
             ventaDetalleResponse = new VentaDetalleResponse("No se encontraron registros del detalle de todas las ventas", 400, false);
@@ -35,8 +40,10 @@ public class VentaDetalleService {
 
     //  INSERT A NEW VENTA DETALLE RECORD
     public ResponseEntity<Object> insertVentaDetalle(final VentaDetalle ventaDetalle) {
+        logger.debug("====================="+ventaDetalle.toString());
         this.ventaDetalleRepository.save(ventaDetalle);
-        ventaDetalleResponse = new VentaDetalleResponse(ventaDetalle, "Se pudo crear el detalle de la venta", 200, true);
+        ventaDetalleResponse = new VentaDetalleResponse(ventaDetalle,
+                "Se pudo crear el detalle de la venta", 200, true);
         return new ResponseEntity<>(ventaDetalleResponse.response(), HttpStatus.OK);
     } // close method
 
@@ -50,7 +57,8 @@ public class VentaDetalleService {
             existingVentaDetalle.setPrecio(ventaDetalle.getPrecio());
             existingVentaDetalle.setSub_total(ventaDetalle.getSub_total());
             ventaDetalleRepository.save(existingVentaDetalle);
-            ventaDetalleResponse = new VentaDetalleResponse(existingVentaDetalle, "Se pudo actualizar", 200, true);
+            ventaDetalleResponse = new VentaDetalleResponse(existingVentaDetalle,
+                    "Se pudo actualizar", 200, true);
             return new ResponseEntity<>(ventaDetalleResponse.response(), HttpStatus.OK);
         } else {
             ventaDetalleResponse = new VentaDetalleResponse("No existe el ID: " + id, 400, false);
