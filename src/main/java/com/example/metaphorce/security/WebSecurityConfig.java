0f7d9ec5dbacaee93755e25122680253a8cc83e1
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-10-22 22:29:05 -0600

    ADD: basic auth

--- src/main/java/com/example/metaphorce/security/WebSecurityConfig.java
+++ src/main/java/com/example/metaphorce/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.metaphorce.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,16 +13,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+//@AllArgsConstructor
 public class WebSecurityConfig {
 
+    /*private final UserDetailsService userDetailsService;
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;*/
+
     @Bean
-    SecurityFilterChain filterChain(final HttpSecurity httpSecurity,
-                                    final AuthenticationManager authenticationManager) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity,
+                                    AuthenticationManager authenticationManager) throws Exception {
+
+        /*JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter();
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");*/
+
         return httpSecurity
                 .csrf().disable()
-                .authorizeHttpRequests()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -30,30 +41,52 @@ public class WebSecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                /*.addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)*/
                 .build();
     } //close method
 
+    /**
+     * Metodo que hardcodea las credenciales de usuario
+     * para el login de la api
+     *
+     * @return el manager con las credenciales
+     */
     @Bean
     UserDetailsService userDetailsService() {
+        //  Carga usuarios en memoria
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("admin")
                 .password(passwordEncoder().encode("admin"))
                 .roles()
                 .build());
         return manager;
-    }   //close method
+    } //close method
 
+    /**
+     * 
+     *
+     * @param httpSecurity
+     * @param passwordEncoder
+     * @return
+     * @throws Exception
+     */
     @Bean
-    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity,
+                                                PasswordEncoder passwordEncoder) throws Exception {
         return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
                 .userDetailsService(userDetailsService())
                 .passwordEncoder(passwordEncoder())
-                .and().build();
-
+                .and()
+                .build();
     } //close method
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     } //close method
+
+    /*public static void main(String[] args) {
+        System.out.println("pass: " + new BCryptPasswordEncoder().encode("contrasena1"));
+    }*/
 } //close class
