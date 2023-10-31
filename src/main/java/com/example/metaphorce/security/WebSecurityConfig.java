06aae0e311db519c8773ad096f2b7afa1b14f7e6
Luis Gerardo  López Ruiz <108818400+GeraTaker@users.noreply.github.com>
2023-10-30 22:26:55 -0600

    ADD

--- src/main/java/com/example/metaphorce/security/WebSecurityConfig.java
+++ src/main/java/com/example/metaphorce/security/WebSecurityConfig.java
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -54,8 +54,7 @@ public class WebSecurityConfig {
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/ruta-publica").permitAll()
-                .antMatchers("/ruta-protegida").authenticated()
+
                 .and()
                 .httpBasic()
                 .and()
