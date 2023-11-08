1fb4064f80f14fc6a0359b4726faa7cfe6d57f15
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-07 23:21:59 -0600

    REFACTOR: codigo basura y formateo

--- src/main/java/com/example/metaphorce/repository/RolRepository.java
+++ src/main/java/com/example/metaphorce/repository/RolRepository.java
@@ -4,9 +4,7 @@ import com.example.metaphorce.model.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface RolRepository extends JpaRepository<Rol,Long> {
-    //Optional<Rol> findByName(String name);
+public interface RolRepository extends JpaRepository<Rol, Long> {
+
 }
