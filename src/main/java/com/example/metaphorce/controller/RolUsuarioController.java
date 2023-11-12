d946f08bad9262ab3279012988384d20de7bdcc3
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 14:44:48 -0600

    ADD: roles /rolUsuario

--- src/main/java/com/example/metaphorce/controller/RolUsuarioController.java
+++ src/main/java/com/example/metaphorce/controller/RolUsuarioController.java
@@ -3,6 +3,7 @@ package com.example.metaphorce.controller;
 import com.example.metaphorce.model.RolUsuario;
 import com.example.metaphorce.service.RolUsuarioService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,27 +16,32 @@ public class RolUsuarioController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> getRolesUsuarios() {
         return rolUsuarioService.getRolesUsuarios();
     } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> getRolUsuario(@PathVariable Long id) {
         return this.rolUsuarioService.getOne(id);
     } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> registrarRolUsuario(@RequestBody RolUsuario rolUsuario) {
         return this.rolUsuarioService.newRolUsuario(rolUsuario);
     } //close method
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> actualizarRolUsuario(@PathVariable Long id,
                                                        @RequestBody RolUsuario rolUsuario) {
         return this.rolUsuarioService.updateRolUsuario(id, rolUsuario);
     } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> eliminarRolUsuario(@PathVariable Long id) {
         return this.rolUsuarioService.eliminar(id);
     } //close method
