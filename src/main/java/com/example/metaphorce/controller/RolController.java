e61c26d4b30e9c131f2e9fee97f91b5a4c550cac
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 10:38:58 -0600

    ADD: roles en /rol

--- src/main/java/com/example/metaphorce/controller/RolController.java
+++ src/main/java/com/example/metaphorce/controller/RolController.java
@@ -1,43 +1,46 @@
 package com.example.metaphorce.controller;
 
-        import com.example.metaphorce.model.Rol;
-        import com.example.metaphorce.service.RolService;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.security.access.prepost.PreAuthorize;
-        import org.springframework.web.bind.annotation.*;
+import com.example.metaphorce.model.Rol;
+import com.example.metaphorce.service.RolService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/rol")
 public class RolController {
     private final RolService rolService;
 
-    public RolController(RolService rolService){
+    public RolController(RolService rolService) {
         this.rolService = rolService;
     }
 
     @GetMapping("/all")
     @PreAuthorize("hasRole('Admin')")
-    public ResponseEntity<Object> getRoles(){
+    public ResponseEntity<Object> getRoles() {
         return rolService.getRoles();
     }
 
     @GetMapping("/getOne/{id}")
-    public ResponseEntity<Object> getRol(@PathVariable Long id){
+    @PreAuthorize("hasRole('Admin')")
+    public ResponseEntity<Object> getRol(@PathVariable Long id) {
         return this.rolService.getOne(id);
     }
 
     @PostMapping("/create")
     @PreAuthorize("hasAuthority('ROLE_Admin')")
-    public ResponseEntity<Object> registrarRol(@RequestBody Rol rol){
+    public ResponseEntity<Object> registrarRol(@RequestBody Rol rol) {
         return this.rolService.newRol(rol);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Object> actualizarRol(@PathVariable Long id, @RequestBody Rol rol){
+    @PreAuthorize("hasRole('Admin')")
+    public ResponseEntity<Object> actualizarRol(@PathVariable Long id, @RequestBody Rol rol) {
         return this.rolService.updateRol(id, rol);
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> eliminarRol(@PathVariable Long id) {
         return this.rolService.eliminar(id);
     }
