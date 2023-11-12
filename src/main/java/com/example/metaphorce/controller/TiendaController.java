be2acbfee324953b079f9818af13a726c091ba60
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 14:40:56 -0600

    ADD: roles /tienda

--- src/main/java/com/example/metaphorce/controller/TiendaController.java
+++ src/main/java/com/example/metaphorce/controller/TiendaController.java
@@ -4,6 +4,7 @@ import com.example.metaphorce.model.Tienda;
 import com.example.metaphorce.service.TiendaServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,26 +20,31 @@ public class TiendaController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('Admin','Supplier','Comprador')")
     public ResponseEntity<Object> getTienda() {
         return tiendaServices.getTienda();
     } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Supplier','Comprador')")
     public ResponseEntity<Object> getTienda(@PathVariable Integer id) {
         return this.tiendaServices.getOne(id);
     } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> registrarTienda(@RequestBody Tienda tienda) {
         return this.tiendaServices.newTienda(tienda);
     } //close method
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> actualizarTienda(@PathVariable Integer id, @RequestBody Tienda tienda) {
         return this.tiendaServices.updateTienda(id, tienda);
     } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> eliminarUsuario(@PathVariable Integer id) {
         return this.tiendaServices.eliminar(id);
     } //close method
