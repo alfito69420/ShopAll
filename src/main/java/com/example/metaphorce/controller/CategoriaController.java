c82574b9ffa0ff3c144c9f0c7c1abbd999360fca
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 13:17:52 -0600

    ADD: rol a las categorias

--- src/main/java/com/example/metaphorce/controller/CategoriaController.java
+++ src/main/java/com/example/metaphorce/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import com.example.metaphorce.model.Categoria;
 import com.example.metaphorce.service.CategoriaServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,35 +13,39 @@ import org.springframework.web.bind.annotation.*;
 public class CategoriaController {
     private final CategoriaServices categoriaServices;
 
-
     @Autowired
     public CategoriaController(CategoriaServices categoriaServices) {
         this.categoriaServices = categoriaServices;
     }
 
+    //@PreAuthorize("hasRole('Comprador') OR hasRole('Supplier') OR ")
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('Comprador','Admin','Supplier')")
     public ResponseEntity<Object> getCategoria() {
         return categoriaServices.getCategoria();
-    }
+    } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasAnyRole('Comprador','Admin','Supplier')")
     public ResponseEntity<Object> getCategoria(@PathVariable Long id) {
         return this.categoriaServices.getOne(id);
-    }
+    } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasAnyRole('Comprador','Admin','Supplier')")
     public ResponseEntity<Object> registrarCategoria(@RequestBody Categoria categoria) {
         return this.categoriaServices.newCategoria(categoria);
     }
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> actualizarCategoria(@PathVariable Long id, @RequestBody Categoria categoria) {
         return this.categoriaServices.updateCategoria(id, categoria);
-    }
+    } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> eliminarUsuario(@PathVariable Long id) {
         return this.categoriaServices.eliminar(id);
-
-    }
-}
+    } //close method
+} //close class
