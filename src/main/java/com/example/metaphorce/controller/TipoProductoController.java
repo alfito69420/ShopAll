f345d9e2789514a18ef52517c75af6083f9a2c96
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 14:48:15 -0600

    ADD: roles /tipoProducto

--- src/main/java/com/example/metaphorce/controller/TipoProductoController.java
+++ src/main/java/com/example/metaphorce/controller/TipoProductoController.java
@@ -5,6 +5,7 @@ import com.example.metaphorce.model.TipoProducto;
 import com.example.metaphorce.service.TipoProductoServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,27 +19,32 @@ public class TipoProductoController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getTipoProducto() {
         return tipoProductoServices.getTipoProducto();
     } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getTipoProducto(@PathVariable Long id) {
         return this.tipoProductoServices.getOne(id);
     } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> registrarTipoProducto(@RequestBody TipoProducto tipoProducto) {
         return this.tipoProductoServices.newTipoProducto(tipoProducto);
     } //close method
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> actualizarTipoProducto(@PathVariable Long id,
                                                          @RequestBody TipoProducto tipoProducto) {
         return this.tipoProductoServices.updateTipoProducto(id, tipoProducto);
     } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> eliminarUsuario(@PathVariable Long id) {
         return this.tipoProductoServices.eliminar(id);
     } //close method
