fe104b64fdf9f933b7c6ec6490d2a7fc8dc6cbbd
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 14:47:24 -0600

    ADD: roles /tipoPago

--- src/main/java/com/example/metaphorce/controller/TipoPagoController.java
+++ src/main/java/com/example/metaphorce/controller/TipoPagoController.java
@@ -3,6 +3,7 @@ package com.example.metaphorce.controller;
 import com.example.metaphorce.model.TipoPago;
 import com.example.metaphorce.service.TipoPagoServices;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,26 +17,31 @@ public class TipoPagoController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getTipoPago() {
         return tipoPagoServices.getTipoPago();
     } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getTipoPago(@PathVariable Long id) {
         return this.tipoPagoServices.getOne(id);
     } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> registrarTipoPago(@RequestBody TipoPago tipoPago) {
         return this.tipoPagoServices.newTipoPago(tipoPago);
     } //close method
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> actualizarTipoPago(@PathVariable Long id, @RequestBody TipoPago tipoPago) {
         return this.tipoPagoServices.updateTipoPago(id, tipoPago);
     } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> eliminarUsuario(@PathVariable Long id) {
         return this.tipoPagoServices.eliminar(id);
     } //close method
