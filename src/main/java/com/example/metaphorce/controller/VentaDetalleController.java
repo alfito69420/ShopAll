1855a270adfe1a2b9d6384f7bacdb436c444f6fa
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 14:46:14 -0600

    ADD: roles /ventaDetalle

--- src/main/java/com/example/metaphorce/controller/VentaDetalleController.java
+++ src/main/java/com/example/metaphorce/controller/VentaDetalleController.java
@@ -4,6 +4,7 @@ import com.example.metaphorce.model.VentaDetalle;
 import com.example.metaphorce.service.VentaDetalleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,27 +19,32 @@ public class VentaDetalleController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> getAllVentaDetalle() {
         return ventaDetalleService.getVentaDetalle();
     } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getOneVentaDetalle(@PathVariable final Long id) {
         return this.ventaDetalleService.getOneVentaDetalle(id);
     } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> insertVentaDetalle(@RequestBody final VentaDetalle ventaDetalle) {
         return this.ventaDetalleService.insertVentaDetalle(ventaDetalle);
     } //close method
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> updateVentaDetalle(@PathVariable final Long id,
                                                      @RequestBody final VentaDetalle ventaDetalle) {
         return this.ventaDetalleService.updateVentaDetalle(id, ventaDetalle);
     } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> deleteVentaDetalle(@PathVariable final Long id) {
         return this.ventaDetalleService.deleteVentaDetalle(id);
     } //close method
