75303239a0c8a0a5ad0753b744f6126ccb90c127
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 14:34:21 -0600

    ADD: roles /resena

--- src/main/java/com/example/metaphorce/controller/ResenaController.java
+++ src/main/java/com/example/metaphorce/controller/ResenaController.java
@@ -4,6 +4,7 @@ import com.example.metaphorce.model.Resena;
 import com.example.metaphorce.service.ResenaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,26 +19,31 @@ public class ResenaController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getResena() {
         return resenaService.getResena();
     } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getResena(@PathVariable Long id) {
         return this.resenaService.getOne(id);
     } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> registrarResena(@RequestBody Resena resena) {
         return this.resenaService.newResena(resena);
     } //close method
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> actualizarResena(@PathVariable Long id, @RequestBody Resena resena) {
         return this.resenaService.updateResena(id, resena);
     } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> eliminarResena(@PathVariable Long id) {
         return this.resenaService.eliminarResena(id);
     } //close method
