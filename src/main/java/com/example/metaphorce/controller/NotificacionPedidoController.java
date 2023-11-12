75dde35ca458c73801637f85631b32187f1acc5a
Alfredo Aramburo <panchopistolas159753@gmail.com>
2023-11-12 14:50:26 -0600

    ADD: roles /NotificacionPedido

--- src/main/java/com/example/metaphorce/controller/NotificacionPedidoController.java
+++ src/main/java/com/example/metaphorce/controller/NotificacionPedidoController.java
@@ -3,6 +3,7 @@ package com.example.metaphorce.controller;
 import com.example.metaphorce.model.NotificacionPedido;
 import com.example.metaphorce.service.NotificacionPedidoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,27 +16,32 @@ public class NotificacionPedidoController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getNotificacionesPedidos() {
         return notificacionPedidoService.getNotificacionesPedidos();
     } //close method
 
     @GetMapping("/getOne/{id}")
+    @PreAuthorize("hasAnyRole('Admin','Comprador')")
     public ResponseEntity<Object> getNotificacionPedido(@PathVariable Long id) {
         return this.notificacionPedidoService.getOne(id);
     } //close method
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> registrarNotificacionPedido(@RequestBody NotificacionPedido notificacionPedido) {
         return this.notificacionPedidoService.newNotificacionPedido(notificacionPedido);
     } //close method
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> actualizarNotificacionPedido(@PathVariable Long id,
                                                                @RequestBody NotificacionPedido notificacionPedido) {
         return this.notificacionPedidoService.updateNotificacionPedido(id, notificacionPedido);
     } //close method
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Object> eliminarNotificacionPedido(@PathVariable Long id) {
         return this.notificacionPedidoService.eliminar(id);
     } //close method
